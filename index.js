08764f017b392bf19ca2d9260a7a543ca48c100f
dbusa429 <darshitbusa8@gmail.com>
2021-01-28 12:18:16 +0530

    bug fixed

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const bodyParser = require('body-parser');
 const route = require("./route");
-mongoose.connect("mongodb://localhost:27017/emp",{useNewUrlParser:true,useUnifiedTopology:true}).then(
+const { json } = require('body-parser');
+mongoose.connect("mongodb+srv://user1:apstndp16@cluster0.vqjzb.mongodb.net/students?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true}).then(
     ()=>{
         const app = express();
         app.use("/api",route);
-        app.listen(3000,()=>{
+        app.use(bodyParser.urlencoded({extended: true}));
+        app.use(express.json());
+        app.listen(process.env.PORT||3000,()=>{
             console.log("Server Started on 3000 !!");
         });
     }
--- route.js
+++ route.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const Book = require('./Model/book');
 const router = express.Router();
+router.use(bodyParser.urlencoded({extended:true}));
+router.use(express.json());
 
 router.get("/books",async(req,res)=>{
     const book = await Book.find();
     res.send(book);
+});
+router.post("/books",async(req,res)=>{
+    const book =new Book({
+        name:req.body.name,
+        qty:req.body.qty
+
+    });
+    await book.save();
+    res.send();
 });
 module.exports = router;
